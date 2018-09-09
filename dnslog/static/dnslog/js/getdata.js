32d4589689402923a3c0735830025a79a31b3d00
imjdl <imelloit@gmail.com>
2018-09-09 09:08:53 +0800

    异步获取数据完成

--- dnslog/static/dnslog/js/getdata.js
+++ dnslog/static/dnslog/js/getdata.js
@@ -15,21 +15,52 @@ function strtojson(str) {
     var json = (new Function("return " + str))();
     return json;
 }
-
+function clear_log() {
+    var xmlhttp = getXmlHttp();
+    xmlhttp.open("GET",'/index/deldata', true);
+    xmlhttp.send();
+}
 function getLogdata() {
     var xmlhttp = getXmlHttp();
+    var dnslog = document.getElementById('dnslog');
     var csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value;
     xmlhttp.open("POST",'/index/getdata', true);
     xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
     var data = "csrfmiddlewaretoken=" + csrf;
     xmlhttp.send(data)
     xmlhttp.onreadystatechange = function() {
-        var data = strtojson(xmlhttp.responseText).logdata;
-        if(data == '[]'){
-            console.log(123);
-        }else {
-            console.log(data);
-        }
+         if (xmlhttp.readyState==4 && xmlhttp.status==200){
+             var logdata = strtojson(xmlhttp.responseText)['logdata'];
+
+             if (logdata.length == 0){
+                dnslog.innerHTML = "<div class=\"container\">\n" +
+                    "        <h1 class=\"text-info text-center\">Sorry no data!!</h1>\n" +
+                    "   </div>";
+             }else {
+                var mytable = "<table class=\"table\">\n" +
+                    "    <caption>DNSLOG平台</caption>\n" +
+                    "    <thead>\n" +
+                    "        <tr>\n" +
+                    "            <th>IP</th>\n" +
+                    "            <th>qname</th>\n" +
+                    "            <th>qtype</th>\n" +
+                    "            <th>时间</th>\n" +
+                    "        </tr>\n" +
+                    "    </thead>\n" +
+                    "    <tbody>\n";
+                for(var i=0; i<logdata.length; i++){
+                    var data = "<tr>\n" +
+                        "            <td>"+ logdata[i].IP +"</td>\n" +
+                        "            <td>" +logdata[i].text+ "</td>\n" +
+                        "            <td>"+logdata[i].qtype+"</td>\n" +
+                        "            <td>"+logdata[i].recvdate+"</td>\n" +
+                        "        </tr>"
+                    mytable += data;
+                }
+                mytable += "</tbody></table>";
+                dnslog.innerHTML = mytable;
+             }
+         }
     }
 
 }
